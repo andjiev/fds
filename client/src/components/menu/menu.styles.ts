018e470631c73073d7f30516f2f0c2db33487f19
Ivan Andjiev <ivan.andjiev@hotmail.com>
2023-12-12 02:15:13 +0100

    Delete client/src/components/menu directory

--- client/src/components/menu/menu.styles.ts
+++
@@ -1,9 +0,0 @@
-import { Box } from '@mui/material';
-import styled from 'styled-components';
-
-const StyledBox = styled(Box)`
-  background-color: #ffffff;
-  width: 100%;
-`;
-
-export { StyledBox };
