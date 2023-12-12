268cd552cf3097aa0e5fdea9e014c5cbab398241
Ivan Andjiev <ivan.andjiev@gmail.com>
2023-12-12 01:49:35 +0100

    lazy fix

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -7,8 +7,9 @@ import { bootstrapApp } from './store/shared-store';
 import Loading from './components/Loading';
 import { onGetPackages } from './store/package-store';
 import { ToastContainer, toast } from 'react-toastify';
+import Menu from './components/Menu';
 
-const Menu = lazy(() => import('./components/Menu'));
+// const Menu = lazy(() => import('./components/Menu'));
 
 const App = () => {
   const dispatch = useAppDispatch();
@@ -24,7 +25,7 @@ const App = () => {
   return (
     <>
       {applicationBootstraped ? (
-        <Suspense fallback={renderLoader()}>
+        // <Suspense fallback={renderLoader()}>
           <Box style={{ backgroundColor: 'rgb(237, 238, 240)', minHeight: '100vh' }}>
             <Menu />
             <Box ml={3} mr={3}>
@@ -32,7 +33,7 @@ const App = () => {
               <ToastContainer position="top-right" theme="colored" />
             </Box>
           </Box>
-        </Suspense>
+        // </Suspense>
       ) : (
         renderLoader()
       )}
